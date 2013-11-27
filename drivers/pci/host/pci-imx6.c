7192d31faf5ae1067d110747ff5154395727cfd3
Richard Zhu <r65037@freescale.com>
2013-11-27 15:24:37 +0800

    ENGR00289889 pcie: make all pcie gpio be optional
    make all pcie gpio be optional, since
    ard board doesn't have pcie reset gpio pin
    
    Signed-off-by: Richard Zhu <r65037@freescale.com>

--- drivers/pci/host/pci-imx6.c
+++ drivers/pci/host/pci-imx6.c
@@ -343,9 +343,11 @@ static int imx6_pcie_wait_for_link(struct pcie_port *pp)
 		dw_pcie_msi_init(pp);
 	}
 
-	gpio_set_value(imx6_pcie->reset_gpio, 0);
-	msleep(100);
-	gpio_set_value(imx6_pcie->reset_gpio, 1);
+	if (gpio_is_valid(imx6_pcie->reset_gpio)) {
+		gpio_set_value(imx6_pcie->reset_gpio, 0);
+		msleep(100);
+		gpio_set_value(imx6_pcie->reset_gpio, 1);
+	}
 
 	return 0;
 }
