b4a5c5635ae9e01f9a866d19d4018a63d05e54eb
Jon Nettleton <jon@laptop.org>
2013-12-10 18:59:47 +0100

    mxc: gpu_viv: Wrap interrupt handling in a spinlock
    Make sure that multiple threads are enabling/disabling irq's
    when we are trying to read events pending.

--- drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_device.c
+++ drivers/mxc/gpu-viv/hal/os/linux/kernel/gc_hal_kernel_device.c
@@ -1270,12 +1270,14 @@ gckGALDEVICE_Enable_ISR(
         gcmkONERROR(gcvSTATUS_GENERIC_IO);
     }
 
+    spin_lock(&Device->kernels[Core]->irq_lock);
     if (Device->isrEnabled[Core] == 0)
     {
         enable_irq(Device->irqLines[Core]);
         /* Mark ISR as initialized. */
         Device->isrEnabled[Core] = gcvTRUE;
     }
+    spin_unlock(&Device->kernels[Core]->irq_lock);
 
     gcmkFOOTER_NO();
     return gcvSTATUS_OK;
@@ -1341,12 +1343,14 @@ gckGALDEVICE_Disable_ISR(
     gcmkVERIFY_ARGUMENT(Device != NULL);
 
     /* disable the irq */
+    spin_lock(&Device->kernels[Core]->irq_lock);
     if (Device->isrEnabled[Core])
     {
         disable_irq(Device->irqLines[Core]);
 
         Device->isrEnabled[Core] == gcvFALSE;
     }
+    spin_unlock(&Device->kernels[Core]->irq_lock);
 
     gcmkFOOTER_NO();
     return gcvSTATUS_OK;
