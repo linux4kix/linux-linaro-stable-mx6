7cde98a6fc85fb826953b93d555c8c3bbc7f3c46
Russell King <rmk+kernel@arm.linux.org.uk>
2014-02-19 10:41:19 +0100

    mmc: sdhci: remove platform_suspend/platform_resume callbacks
    The only user (sdhci-of-esdhc) no longer uses these callbacks, so lets
    remove them to discourage any further use.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2526,9 +2526,6 @@ int sdhci_suspend_host(struct sdhci_host *host)
 {
 	int ret;
 
-	if (host->ops->platform_suspend)
-		host->ops->platform_suspend(host);
-
 	sdhci_disable_card_detection(host);
 
 	/* Disable tuning since we are suspending */
@@ -2598,9 +2595,6 @@ int sdhci_resume_host(struct sdhci_host *host)
 
 	ret = mmc_resume_host(host->mmc);
 	sdhci_enable_card_detection(host);
-
-	if (host->ops->platform_resume)
-		host->ops->platform_resume(host);
 
 	/* Set the re-tuning expiration flag */
 	if (host->flags & SDHCI_USING_RETUNING_TIMER)
