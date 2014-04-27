52bb81bed063475a0f93bbde395e1df252383dda
Andras Biro <bbandi86@gmail.com>
2014-04-28 00:31:39 +0200

    disable interlaced modes

--- drivers/video/mxc/mxc_hdmi.c
+++ drivers/video/mxc/mxc_hdmi.c
@@ -1815,9 +1815,9 @@ static void mxc_hdmi_edid_rebuild_modelist(struct mxc_hdmi *hdmi)
 		 */
 		mode = &hdmi->fbi->monspecs.modedb[i];
 
-		if (hdmi->edid_cfg.hdmi_cap &&
-		    (mode->vmode & FB_VMODE_INTERLACED) &&
-		    (mxc_edid_mode_to_vic(mode) == 0))
+		if ((mode->vmode & FB_VMODE_INTERLACED) ||
+		    (hdmi->edid_cfg.hdmi_cap &&
+		    (mxc_edid_mode_to_vic(mode) == 0)))
 			continue;
 
 		dev_dbg(&hdmi->pdev->dev, "Added mode %d:", i);
