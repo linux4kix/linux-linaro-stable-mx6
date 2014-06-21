18255f29c570e2f91982822e27dc70caea86ce3c
Christian Gmeiner <christian.gmeiner@gmail.com>
2014-06-21 19:01:17 +0200

    move command stream helpers into their own file
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

---
+++ drivers/staging/vivante/vivante_command.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2014 2014 Etnaviv Project
+ * Author: Christian Gmeiner <christian.gmeiner@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "vivante_ringbuffer.h"
+
+#include "common.xml.h"
+#include "state.xml.h"
+#include "cmdstream.xml.h"
+
+/*
+ * Command Buffer helper:
+ */
+
+static inline void CMD_LOAD_STATE(struct vivante_ringbuffer *rb, u32 reg, u32 value)
+{
+	/* write a register via cmd stream */
+	OUT_RING(rb, VIV_FE_LOAD_STATE_HEADER_OP_LOAD_STATE | VIV_FE_LOAD_STATE_HEADER_COUNT(1) |
+			VIV_FE_LOAD_STATE_HEADER_OFFSET(reg >> VIV_FE_LOAD_STATE_HEADER_OFFSET__SHR));
+	OUT_RING(rb, value);
+}
+
+static inline void CMD_END(struct vivante_ringbuffer *rb)
+{
+	OUT_RING(rb, VIV_FE_END_HEADER_OP_END);
+}
+
+static inline void CMD_NOP(struct vivante_ringbuffer *rb)
+{
+	OUT_RING(rb, VIV_FE_NOP_HEADER_OP_NOP);
+}
+
+static inline void CMD_WAIT(struct vivante_ringbuffer *rb)
+{
+	OUT_RING(rb, VIV_FE_WAIT_HEADER_OP_WAIT | 200);
+}
+
+static inline void CMD_LINK(struct vivante_ringbuffer *rb, u16 prefetch, u32 address)
+{
+	OUT_RING(rb, VIV_FE_LINK_HEADER_OP_LINK | VIV_FE_LINK_HEADER_PREFETCH(prefetch));
+	OUT_RING(rb, address);
+}
+
+static inline void CMD_STALL(struct vivante_ringbuffer *rb, u32 from, u32 to)
+{
+	OUT_RING(rb, VIV_FE_STALL_HEADER_OP_STALL);
+	OUT_RING(rb, VIV_FE_STALL_TOKEN_FROM(from) | VIV_FE_STALL_TOKEN_TO(to));
+}
+
+
+void vivante_cmd_select_pipe(struct vivante_ringbuffer *rb, u8 pipe)
+{
+	u32 flush;
+	u32 stall;
+
+	if (pipe == 0x1) /* 2d */
+		flush = VIVS_GL_FLUSH_CACHE_DEPTH | VIVS_GL_FLUSH_CACHE_COLOR;
+	else
+		flush = VIVS_GL_FLUSH_CACHE_TEXTURE;
+
+	stall = VIVS_GL_SEMAPHORE_TOKEN_FROM(SYNC_RECIPIENT_FE) |
+			VIVS_GL_SEMAPHORE_TOKEN_TO(SYNC_RECIPIENT_PE);
+
+	CMD_LOAD_STATE(rb, VIVS_GL_FLUSH_CACHE, flush);
+	CMD_LOAD_STATE(rb, VIVS_GL_SEMAPHORE_TOKEN, stall);
+
+	CMD_STALL(rb, SYNC_RECIPIENT_FE, SYNC_RECIPIENT_PE);
+
+	/* TODO: pipe2d = 0x1 pipe3d = 0x0 */
+	CMD_LOAD_STATE(rb, VIVS_GL_PIPE_SELECT, VIVS_GL_PIPE_SELECT_PIPE(pipe));
+}
