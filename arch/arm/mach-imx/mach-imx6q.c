boundarydevices/linux

ENGR00286041 arm: imx: avoid warning message of speed grading check on i.MX6DL
i.MX6DL did NOT have 1.2GHz setpoint, no need to check speed
grading fuse for 1.2GHz opp, otherwise, there will be always
warning message of "failed to disable 1.2 GHz OPP" when system
boot up on i.MX6DL.

Signed-off-by: Anson Huang <b20788@freescale.com>

arch/arm/mach-imx/mach-imx6q.c
@@ -364,7 +364,7 @@ static void __init imx6q_opp_check_speed_grading(struct device *cpu_dev)
 
 	/*
 	 * SPEED_GRADING[1:0] defines the max speed of ARM:
-	 * 2b'11: 1200000000Hz;
+	 * 2b'11: 1200000000Hz; -- i.MX6Q only.
 	 * 2b'10: 1000000000Hz;
 	 * 2b'01: 850000000Hz; -- i.MX6Q Only, exclusive with 1GHz.
 	 * 2b'00: 800000000Hz;
@@ -372,9 +372,11 @@ static void __init imx6q_opp_check_speed_grading(struct device *cpu_dev)
 	 */
 	val = readl_relaxed(base + OCOTP_CFG3);
 	val >>= OCOTP_CFG3_SPEED_SHIFT;
-	if ((val & 0x3) < OCOTP_CFG3_SPEED_1P2GHZ)
-		if (opp_disable(cpu_dev, 1200000000))
-			pr_warn("failed to disable 1.2 GHz OPP\n");
+	if (cpu_is_imx6q()) {
+		if ((val & 0x3) < OCOTP_CFG3_SPEED_1P2GHZ)
+			if (opp_disable(cpu_dev, 1200000000))
+				pr_warn("failed to disable 1.2 GHz OPP\n");
+	}
 	if ((val & 0x3) < OCOTP_CFG3_SPEED_1GHZ)
 		if (opp_disable(cpu_dev, 996000000))
 			pr_warn("failed to disable 1 GHz OPP\n");

linux4kix/linux-linaro-stable-mx6

arch/arm/mach-imx/mach-imx6q.c
@@ -374,7 +374,7 @@ static void __init imx6q_opp_check_speed_grading(struct device *cpu_dev)
 
 	/*
 	 * SPEED_GRADING[1:0] defines the max speed of ARM:
-	 * 2b'11: 1200000000Hz;
+	 * 2b'11: 1200000000Hz; -- i.MX6Q only.
 	 * 2b'10: 1000000000Hz;
 	 * 2b'01: 850000000Hz; -- i.MX6Q Only, exclusive with 1GHz.
 	 * 2b'00: 800000000Hz;
@@ -383,9 +383,11 @@ static void __init imx6q_opp_check_speed_grading(struct device *cpu_dev)
 
 	val = readl_relaxed(base + OCOTP_CFG3);
 	val >>= OCOTP_CFG3_SPEED_SHIFT;
-	if ((val & 0x3) < OCOTP_CFG3_SPEED_1P2GHZ)
-		if (dev_pm_opp_disable(cpu_dev, 1200000000))
-			pr_warn("failed to disable 1.2 GHz OPP\n");
+	if (cpu_is_imx6q()) {
+		if ((val & 0x3) < OCOTP_CFG3_SPEED_1P2GHZ)
+			if (dev_pm_opp_disable(cpu_dev, 1200000000))
+				pr_warn("failed to disable 1.2 GHz OPP\n");
+	}
 	if ((val & 0x3) < OCOTP_CFG3_SPEED_1GHZ)
 		if (dev_pm_opp_disable(cpu_dev, 996000000))
 			pr_warn("failed to disable 1 GHz OPP\n");