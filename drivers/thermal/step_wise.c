9963c50e462b246bbe309f67f76e8d88123e41b7
Jon Nettleton <jon.nettleton@gmail.com>
2014-11-26 18:40:46 +0100

    thermal: step_wise: Add and fix debug output
    Add some additional debug output to track the changes being
    detected by the driver.  Also fix the typing of some variables
    to verify that they print out properly in the debug output.

--- drivers/thermal/step_wise.c
+++ drivers/thermal/step_wise.c
@@ -71,10 +71,12 @@ static unsigned long get_target_state(struct thermal_instance *instance,
 			if (next_target < instance->lower)
 				next_target = instance->lower;
 		}
+		dev_dbg(&cdev->device, "THERMAL_TREND_RAISING: next_target=%ld\n", next_target);
 		break;
 	case THERMAL_TREND_RAISE_FULL:
 		if (throttle)
 			next_target = instance->upper;
+		dev_dbg(&cdev->device, "THERMAL_TREND_RAISE_FULL: next_target=%ld\n", next_target);
 		break;
 	case THERMAL_TREND_DROPPING:
 		if (cur_state == instance->lower) {
@@ -85,6 +87,7 @@ static unsigned long get_target_state(struct thermal_instance *instance,
 			if (next_target > instance->upper)
 				next_target = instance->upper;
 		}
+		dev_dbg(&cdev->device, "THERMAL_TREND_DROPPING: next_target=%ld\n", next_target);
 		break;
 	case THERMAL_TREND_DROP_FULL:
 		if (cur_state == instance->lower) {
@@ -92,6 +95,7 @@ static unsigned long get_target_state(struct thermal_instance *instance,
 				next_target = THERMAL_NO_TARGET;
 		} else
 			next_target = instance->lower;
+		dev_dbg(&cdev->device, "THERMAL_TREND_DROP_FULL: next_target=%ld\n", next_target);
 		break;
 	default:
 		break;
@@ -118,7 +122,7 @@ static void thermal_zone_trip_update(struct thermal_zone_device *tz, int trip)
 	enum thermal_trend trend;
 	struct thermal_instance *instance;
 	bool throttle = false;
-	int old_target;
+	unsigned long old_target;
 
 	if (trip == THERMAL_TRIPS_NONE) {
 		trip_temp = tz->forced_passive;
@@ -146,8 +150,8 @@ static void thermal_zone_trip_update(struct thermal_zone_device *tz, int trip)
 
 		old_target = instance->target;
 		instance->target = get_target_state(instance, trend, throttle);
-		dev_dbg(&instance->cdev->device, "old_target=%d, target=%d\n",
-					old_target, (int)instance->target);
+		dev_dbg(&instance->cdev->device, "old_target=%ld, target=%ld\n",
+					old_target, instance->target);
 
 		if (old_target == instance->target)
 			continue;
