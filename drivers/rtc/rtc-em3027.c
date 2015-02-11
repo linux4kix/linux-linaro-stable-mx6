4dbe40ab6f1c613523255cc35b5d7f51ef356553
Thomas Genty <tomlohave@openbricks.org>
2015-02-11 19:43:26 +0100

    add a missing section in rtc-em3027.c

--- drivers/rtc/rtc-em3027.c
+++ drivers/rtc/rtc-em3027.c
@@ -150,6 +150,7 @@ static struct i2c_driver em3027_driver = {
 	.driver = {
 		   .name = "rtc-em3027",
 		   .of_match_table = of_match_ptr(em3027_of_match),
+		   .owner	= THIS_MODULE,
 	},
 	.probe = &em3027_probe,
 	.id_table = em3027_id,
